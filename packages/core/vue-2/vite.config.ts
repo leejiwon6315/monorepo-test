403ad45a716f7d6909cf360096c73cff509cb464
leejiwon6315 <heman@dunamu.dev>
2023-10-24 15:13:08 +0900

    chore: vue style, svg 설정

--- packages/core/vue-2/vite.config.ts
+++ packages/core/vue-2/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import { createVuePlugin as vue } from "vite-plugin-vue2";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import path from "path";
-
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
@@ -12,13 +12,27 @@ export default defineConfig({
     dts({
       insertTypesEntry: true, // 컴포넌트 타입 생성
     }),
+    cssInjectedByJsPlugin(), // 빌드시 css를 js에 삽입
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // scss import
+        additionalData:
+          '@import "./src/styles/variables.scss";' +
+          '@import "./src/styles/var.scss";' +
+          '@import"./src/styles/colors.scss";' +
+          '@import "./src/styles/mixins.scss";',
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
+    cssCodeSplit: false,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "heman-core-test-vue-2",
